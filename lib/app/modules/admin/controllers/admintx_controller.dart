ebaf891825d659db8cdaaf2dc154bbf5be5850d2
dna2121 <dnatayawibisono@gmail.com>
2023-06-17 14:44:29 +0700

    try adding data from dropdown to firestore, sukses

--- lib/app/modules/admin/controllers/admintx_controller.dart
+++ lib/app/modules/admin/controllers/admintx_controller.dart
@@ -1,3 +1,56 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:dropdown_search/dropdown_search.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class AdminTxController extends GetxController {}
+import '../../../data/repositories/user_repositories.dart';
+
+class AdminTxController extends GetxController {
+  final popupBuilderKey = GlobalKey<DropdownSearchState<String>>();
+  final trxFormKey = GlobalKey<FormState>();
+  final hargaController = TextEditingController();
+  final userRepo = UserRepository.instance;
+
+  String? selectedValue;
+
+  String? validator(String? value) {
+    return null;
+  }
+
+  void onClose() {
+    hargaController.dispose();
+    super.onClose();
+  }
+
+  Stream<List<String>> streamName() {
+    Query<Map<String, dynamic>> itemsRef = FirebaseFirestore.instance
+        .collection('Users')
+        .orderBy("name", descending: false);
+    return itemsRef.snapshots().map((snapshot) {
+      return snapshot.docs.map((doc) => doc['name'] as String).toList();
+    });
+  }
+
+  void addTrc() async {
+    if (trxFormKey.currentState!.validate()) {
+      String harga = hargaController.text;
+
+      CollectionReference trxCollection =
+          FirebaseFirestore.instance.collection('Transaction');
+
+
+      await trxCollection.add({'harga': harga, 'name': selectedValue});
+
+      Get.defaultDialog(
+        title: 'Success',
+        middleText: "Data added.",
+        onConfirm: () {
+          hargaController.clear();
+          selectedValue = null;
+          Get.back(); //close dialog
+        },
+        textConfirm: 'Okay',
+      );
+    }
+  }
+}
--- lib/app/modules/admin/views/admintx_view.dart
+++ lib/app/modules/admin/views/admintx_view.dart
@@ -1,9 +1,10 @@
+import 'package:dropdown_search/dropdown_search.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:sales_report_app/app/modules/admin/controllers/admintx_controller.dart';
 
 class AdminTxView extends GetView<AdminTxController> {
-  const AdminTxView({super.key});
+  AdminTxView({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -12,10 +13,49 @@ class AdminTxView extends GetView<AdminTxController> {
         title: const Text('Admin'),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text(
-          'AdminTX',
-          style: TextStyle(fontSize: 20),
+      body: Form(
+        key: controller.trxFormKey,
+        child: Padding(
+          padding: const EdgeInsets.all(17.0),
+          child: Column(
+            children: [
+              StreamBuilder(
+                  stream: controller.streamName(),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return Text("Loading...");
+                    }
+
+                    if (snapshot.hasData) {
+                      return DropdownSearch<String>(
+                        key: controller.popupBuilderKey,
+                        popupProps: PopupProps.menu(
+                            showSearchBox: true, showSelectedItems: true),
+                        items: snapshot.data!,
+                        selectedItem: controller.selectedValue,
+                        onChanged: (value) {
+                          controller.selectedValue = value!;
+                        },
+                      );
+                    } else {
+                      return CircularProgressIndicator();
+                    }
+                  }),
+              TextFormField(
+                decoration: InputDecoration(
+                  label: Text("harga"),
+                ),
+                controller: controller.hargaController,
+                validator: controller.validator,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  controller.addTrc();
+                },
+                child: Text("Save"),
+              ),
+            ],
+          ),
         ),
       ),
     );
