f0f459d2ebb6ae46fb9935d3db940dd735fee99d
dna2121 <dnatayawibisono@gmail.com>
2023-06-20 18:32:00 +0700

    retrieve transaction by userid

--- lib/app/modules/transaction/controllers/transaction_controller.dart
+++ lib/app/modules/transaction/controllers/transaction_controller.dart
@@ -1,18 +1,16 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
+import 'package:sales_report_app/app/data/repositories/user_repositories.dart';
 
+import '../../../data/repositories/transactions_repositories.dart';
 import '../../auth/controllers/auth_controller.dart';
 
 class TransactionController extends GetxController {
+  final txRepo = TransactionRepository.instance;
+  final userRepo = UserRepository.instance;
+
   CollectionReference userCollection =
       FirebaseFirestore.instance.collection('Users');
-
-  Stream<QuerySnapshot<Object?>> getTx() {
-    CollectionReference txCollection =
-        FirebaseFirestore.instance.collection('Transaction');
-
-    return txCollection.snapshots();
-  }
 
   Stream<DocumentSnapshot<Object?>> getRole() {
     CollectionReference userCollection =
@@ -20,6 +18,13 @@ class TransactionController extends GetxController {
 
     return userCollection
         .doc('${AuthController.instance.firebaseAuth.currentUser!.uid}')
+        .snapshots();
+  }
+
+  Stream<QuerySnapshot<Object?>> streamTxById() {
+    return txRepo.txCollection
+        .where("userID",
+            isEqualTo: AuthController.instance.firebaseAuth.currentUser!.uid)
         .snapshots();
   }
 }
--- lib/app/modules/transaction/views/transaction_view.dart
+++ lib/app/modules/transaction/views/transaction_view.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: must_be_immutable
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
@@ -52,6 +53,43 @@ class TransactionView extends GetView<TransactionController> {
                 }
               },
             ),
+            Expanded(
+              child: StreamBuilder(
+                  stream: controller.streamTxById(),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasError) {
+                      return Text('Something went wrong');
+                    }
+
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return Text("Loading");
+                    }
+
+                    if (snapshot.hasData) {
+                      return ListView(
+                        children: snapshot.data!.docs
+                            .map((DocumentSnapshot document) {
+                          Map<String, dynamic> data =
+                              document.data()! as Map<String, dynamic>;
+                          return ListTile(
+                            title: Text(data['name']),
+                            subtitle: Text('${data['weight']} ton'),
+                            leading: CircleAvatar(
+                              backgroundColor: Colors.grey[200],
+                              child: Icon(Icons.person),
+                            ),
+                            trailing: Text(
+                              '+Rp ${data['price']}',
+                              style:
+                                  TextStyle(fontSize: 14, color: Colors.green),
+                            ),
+                          );
+                        }).toList(),
+                      );
+                    }
+                    return SizedBox();
+                  }),
+            )
           ],
         ),
       ),
