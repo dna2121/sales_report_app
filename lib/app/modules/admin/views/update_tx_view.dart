57505ecda509e694f90278fa4324f3a1d82cd809
dna2121 <dnatayawibisono@gmail.com>
2023-07-02 14:49:43 +0700

    add singlechildscrollview

--- lib/app/modules/admin/views/update_tx_view.dart
+++ lib/app/modules/admin/views/update_tx_view.dart
@@ -21,107 +21,118 @@ class UpdateTxView extends GetView<AdminTxController> {
           title: const Text('Edit Transaction'),
           centerTitle: true,
         ),
-        body: Form(
-          child: Padding(
-            padding: const EdgeInsets.all(18.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Name',
-                  style: TextStyle(fontSize: 18),
-                ),
-                StreamBuilder(
-                    stream: controller.streamName(),
-                    builder: (context, snapshot) {
-                      if (snapshot.connectionState == ConnectionState.waiting) {
-                        return Text("Loading...");
-                      }
+        body: SingleChildScrollView(
+          child: Form(
+            child: Padding(
+              padding: const EdgeInsets.all(18.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Name',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                  StreamBuilder(
+                      stream: controller.streamName(),
+                      builder: (context, snapshot) {
+                        if (snapshot.connectionState ==
+                            ConnectionState.waiting) {
+                          return Text("Loading...");
+                        }
 
-                      if (snapshot.hasData) {
-                        return DropdownSearch<String>(
-                          clearButtonProps: ClearButtonProps(isVisible: true),
-                          popupProps: PopupProps.menu(
-                              showSearchBox: true,
-                              showSelectedItems: true,
-                              searchFieldProps: TextFieldProps(
-                                  decoration:
-                                      InputDecoration(labelText: "Search..."))),
-                          items: snapshot.data!,
-                          selectedItem: controller.selectedName,
-                          onChanged: (value) async {
-                            controller.selectedName = value;
-                          },
-                        );
-                      } else {
-                        return CircularProgressIndicator();
-                      }
-                    }),
-                SizedBox(height: 17),
-                Text(
-                  'Price',
-                  style: TextStyle(fontSize: 18),
-                ),
-                TextFormField(
-                  controller: controller.priceC,
-                  validator: controller.validator,
-                  keyboardType: TextInputType.number,
-                  decoration: InputDecoration(
-                      prefixText: "Rp. ", border: OutlineInputBorder()),
-                ),
-                SizedBox(height: 17),
-                Text(
-                  'Date',
-                  style: TextStyle(fontSize: 18),
-                ),
-                DateTimeFormField(
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    suffixIcon: Icon(Icons.event_note),
+                        if (snapshot.hasData) {
+                          return DropdownSearch<String>(
+                            clearButtonProps: ClearButtonProps(isVisible: true),
+                            popupProps: PopupProps.menu(
+                                showSearchBox: true,
+                                showSelectedItems: true,
+                                searchFieldProps: TextFieldProps(
+                                    decoration: InputDecoration(
+                                        labelText: "Search..."))),
+                            items: snapshot.data!,
+                            selectedItem: controller.selectedName,
+                            onChanged: (value) async {
+                              controller.selectedName = value;
+                            },
+                            validator: controller.validator,
+                          );
+                        } else {
+                          return CircularProgressIndicator();
+                        }
+                      }),
+                  SizedBox(height: 17),
+                  Text(
+                    'Price',
+                    style: TextStyle(fontSize: 18),
                   ),
-                  initialDate: initialValue,
-                  firstDate: DateTime(1900),
-                  lastDate: DateTime(2100),
-                  initialValue: initialValue,
-                  dateFormat: DateFormat('dd MMMM yyyy'),
-                  autovalidateMode: AutovalidateMode.always,
-                  onDateSelected: (DateTime value) {
-                    controller.dateC.text = value.toString();
-                  },
-                ),
-                SizedBox(height: 17),
-                Text(
-                  'Weight',
-                  style: TextStyle(fontSize: 18),
-                ),
-                TextFormField(
-                  controller: controller.weightC,
-                  validator: controller.validator,
-                  decoration: InputDecoration(
-                      suffixText: "kg ", border: OutlineInputBorder()),
-                ),
-                SizedBox(height: 17),
-                Text(
-                  'Car Number',
-                  style: TextStyle(fontSize: 18),
-                ),
-                TextFormField(
-                  controller: controller.carC,
-                  validator: controller.validator,
-                  decoration: InputDecoration(border: OutlineInputBorder()),
-                  textCapitalization: TextCapitalization.characters,
-                ),
-                SizedBox(height: 17),
-                Container(
-                  width: double.infinity,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      controller.updateTxDoc();
+                  TextFormField(
+                    controller: controller.priceC,
+                    validator: controller.validator,
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                        prefixText: "Rp. ", border: OutlineInputBorder()),
+                  ),
+                  SizedBox(height: 17),
+                  Text(
+                    'Date',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                  DateTimeFormField(
+                    mode: DateTimeFieldPickerMode.date,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      suffixIcon: Icon(Icons.event_note),
+                    ),
+                    initialDate: initialValue,
+                    firstDate: DateTime(1900),
+                    lastDate: DateTime(2100),
+                    initialValue: initialValue,
+                    dateFormat: DateFormat('dd MMMM yyyy'),
+                    autovalidateMode: AutovalidateMode.always,
+                    onDateSelected: (DateTime value) {
+                      controller.dateC.text = value.toString();
                     },
-                    child: Text("Save"),
                   ),
-                ),
-              ],
+                  SizedBox(height: 17),
+                  Text(
+                    'Weight',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                  TextFormField(
+                    controller: controller.weightC,
+                    validator: controller.validator,
+                    decoration: InputDecoration(
+                        suffixText: "kg ", border: OutlineInputBorder()),
+                  ),
+                  SizedBox(height: 17),
+                  Text(
+                    'Car Number',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                  TextFormField(
+                    controller: controller.carC,
+                    validator: controller.validator,
+                    decoration: InputDecoration(border: OutlineInputBorder()),
+                    textCapitalization: TextCapitalization.characters,
+                  ),
+                  SizedBox(height: 17),
+                  Container(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        controller.nameC.text.isEmpty &&
+                                controller.priceC.text.isEmpty &&
+                                controller.dateC.text.isEmpty &&
+                                controller.weightC.text.isEmpty &&
+                                controller.carC.text.isEmpty
+                            ? controller.textEmpty
+                            : controller.updateTxDoc();
+                      },
+                      child: Text("Save"),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ));
