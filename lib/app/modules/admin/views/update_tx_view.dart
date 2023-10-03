b171eaf95811f1c76ffa62776da592deb5dc9f00
dna2121 <dnatayawibisono@gmail.com>
2023-10-03 16:16:23 +0700

    updateTx ui

--- lib/app/modules/admin/views/update_tx_view.dart
+++ lib/app/modules/admin/views/update_tx_view.dart
@@ -20,27 +20,42 @@ class UpdateTxView extends GetView<AdminTxController> {
         DateTime.tryParse(controller.dateC.text) ?? DateTime.now();
 
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Edit Transaction'),
-          centerTitle: true,
-        ),
-        body: SingleChildScrollView(
-          child: Form(
-            child: Padding(
-              padding: const EdgeInsets.all(18.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  TitleText(text: "TransactionID"),
-                  SizedBox(height: 7),
-                  InputField(
-                    readOnly: true,
-                    controller: controller.trxidC,
-                  ),
-                  SizedBox(height: 17),
-                  TitleText(text: "Name"),
-                  SizedBox(height: 7),
-                  StreamBuilder(
+      appBar: AppBar(
+        title: const Text('Ubah Transaksi'),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Container(
+            margin: EdgeInsets.fromLTRB(19, 50, 19, 50),
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(15),
+              border: Border.all(
+                color: Colors.grey.shade300, // Border color
+                width: 1, // Border width
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.3), // Shadow color
+                  spreadRadius: 1,
+                  blurRadius: 70,
+                  offset: Offset(0, 20),
+                ),
+              ],
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 50),
+                Container(
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                      color: AppColor.boxShadow,
+                      borderRadius: BorderRadius.circular(8)),
+                  child: StreamBuilder(
                       stream: controller.streamName(),
                       builder: (context, snapshot) {
                         if (snapshot.connectionState ==
@@ -49,125 +64,153 @@ class UpdateTxView extends GetView<AdminTxController> {
                         }
 
                         if (snapshot.hasData) {
-                          return Container(
-                            decoration: BoxDecoration(
-                                color: AppColor.grey2,
-                                borderRadius: BorderRadius.circular(7)),
-                            child: DropdownSearch<String>(
-                              dropdownDecoratorProps: DropDownDecoratorProps(
-                                dropdownSearchDecoration: InputDecoration(
-                                    hintText: "Select a name",
-                                    border: InputBorder.none,
+                          return DropdownSearch<String>(
+                            dropdownDecoratorProps: DropDownDecoratorProps(
+                              dropdownSearchDecoration: InputDecoration(
+                                  hintText: "Select a name",
+                                  border: InputBorder.none,
+                                  contentPadding: EdgeInsets.all(11)),
+                            ),
+                            clearButtonProps: ClearButtonProps(isVisible: true),
+                            popupProps: PopupProps.dialog(
+                              showSearchBox: true,
+                              showSelectedItems: true,
+                              dialogProps: DialogProps(
+                                backgroundColor: AppColor.background,
+                                contentPadding: EdgeInsetsDirectional.symmetric(
+                                    vertical: 11),
+                              ),
+                              searchFieldProps: TextFieldProps(
+                                padding: EdgeInsets.symmetric(
+                                    horizontal: 17, vertical: 12),
+                                decoration: InputDecoration(
+                                    hintText: "Search...",
                                     contentPadding: EdgeInsets.all(11)),
                               ),
-                              clearButtonProps:
-                                  ClearButtonProps(isVisible: true),
-                              popupProps: PopupProps.dialog(
-                                showSearchBox: true,
-                                showSelectedItems: true,
-                                dialogProps: DialogProps(
-                                  backgroundColor: AppColor.background,
-                                  contentPadding:
-                                      EdgeInsetsDirectional.symmetric(
-                                          vertical: 11),
-                                ),
-                                searchFieldProps: TextFieldProps(
-                                  padding: EdgeInsets.symmetric(
-                                      horizontal: 17, vertical: 12),
-                                  decoration: InputDecoration(
-                                      hintText: "Search...",
-                                      contentPadding: EdgeInsets.all(11)),
-                                ),
-                              ),
-                              items: snapshot.data!,
-                              selectedItem: controller.selectedName,
-                              validator: controller.validator,
-                              onChanged: (value) async {
-                                controller.selectedName = value;
-                                String? userId =
-                                    await controller.getUserIdFromName(value!);
-
-                                controller.carNumberStream =
-                                    controller.streamCarNumber(userId!);
-                              },
                             ),
+                            items: snapshot.data!,
+                            selectedItem: controller.selectedName,
+                            validator: controller.validator,
+                            onChanged: (value) async {
+                              controller.selectedName = value;
+                              // String? userId =
+                              //     await controller.getUserIdFromName(value!);
+
+                              // controller.carNumberStream =
+                              //     controller.streamCarNumber(userId!);
+                            },
                           );
                         } else {
                           return CircularProgressIndicator();
                         }
                       }),
-                  SizedBox(height: 17),
-                  TitleText(text: "Price"),
-                  SizedBox(height: 7),
-                  InputField(
-                    keyboardType: TextInputType.number,
-                    prefixText: "Rp. ",
-                    hintText: "Enter price",
-                    controller: controller.priceC,
-                    validator: controller.validator,
-                  ),
-                  SizedBox(height: 17),
-                  TitleText(text: "Date"),
-                  SizedBox(height: 7),
-                  Container(
-                    decoration: BoxDecoration(
-                        color: AppColor.grey2,
-                        borderRadius: BorderRadius.circular(7)),
-                    child: DateTimeFormField(
-                      mode: DateTimeFieldPickerMode.date,
-                      decoration: const InputDecoration(
-                        contentPadding: EdgeInsets.all(11),
-                        border: InputBorder.none,
-                        suffixIcon: Icon(Icons.event_note),
+                ),
+                SizedBox(height: 20),
+                TxField(
+                  keyboardType: TextInputType.number,
+                  prefixText: "Rp. ",
+                  hintText: "Enter price",
+                  controller: controller.priceC,
+                  validator: controller.validator,
+                ),
+                SizedBox(height: 14),
+                Divider(thickness: 0.5),
+                SizedBox(height: 20),
+                Row(
+                  children: [
+                    Expanded(
+                        child: Text(
+                      "Berat",
+                      style: TextStyle(color: Color.fromRGBO(127, 132, 143, 1)),
+                    )),
+                    Expanded(
+                      flex: 3,
+                      child: TxField(
+                        keyboardType: TextInputType.number,
+                        suffixText: "kg",
+                        hintText: "Enter weight in kilogram",
+                        controller: controller.weightC,
+                        validator: controller.validator,
                       ),
-                      initialDate: initialValue,
-                      firstDate: DateTime(1900),
-                      lastDate: DateTime(2100),
-                      initialValue: initialValue,
-                      dateFormat: DateFormat('dd MMMM yyyy'),
-                      autovalidateMode: AutovalidateMode.always,
-                      onDateSelected: (DateTime value) {
-                        controller.dateC.text = value.toString();
-                      },
                     ),
-                  ),
-                  SizedBox(height: 17),
-                  TitleText(text: "Weight"),
-                  SizedBox(height: 7),
-                  InputField(
-                    keyboardType: TextInputType.number,
-                    suffixText: "kilogram",
-                    hintText: "Enter weight in kilogram",
-                    controller: controller.weightC,
-                    validator: controller.validator,
-                  ),
-                  SizedBox(height: 17),
-                  TitleText(text: "Car Number"),
-                  SizedBox(height: 7),
-                  InputField(
-                    hintText: "ex: KB 1678 QU",
-                    controller: controller.carC,
-                    validator: controller.validator,
-                    textCapitalization: TextCapitalization.characters,
-                  ),
-                  SizedBox(height: 25),
-                  StringButton(
-                      backgroundColor: AppColor.putihBtn,
-                      pressed: () {
-                        controller.nameC.text.isEmpty &&
-                                controller.priceC.text.isEmpty &&
-                                controller.dateC.text.isEmpty &&
-                                controller.weightC.text.isEmpty &&
-                                controller.carC.text.isEmpty
-                            ? controller.textEmpty
-                            : controller.updateTxDoc();
-                      },
-                      text: "Save"),
-                  SizedBox(height: 20),
-                ],
-              ),
+                  ],
+                ),
+                SizedBox(height: 20),
+                Row(
+                  children: [
+                    Expanded(
+                        child: Text(
+                      "Tanggal",
+                      style: TextStyle(color: Color.fromRGBO(127, 132, 143, 1)),
+                    )),
+                    Expanded(
+                      flex: 3,
+                      child: Container(
+                        decoration: BoxDecoration(
+                            color: AppColor.boxShadow,
+                            borderRadius: BorderRadius.circular(7)),
+                        child: DateTimeFormField(
+                          mode: DateTimeFieldPickerMode.date,
+                          decoration: const InputDecoration(
+                            contentPadding: EdgeInsets.all(11),
+                            border: InputBorder.none,
+                            suffixIcon: Icon(Icons.event_note),
+                          ),
+                          initialDate: initialValue,
+                          firstDate: DateTime(1900),
+                          lastDate: DateTime(2100),
+                          initialValue: initialValue,
+                          dateFormat: DateFormat('dd MMM yyyy'),
+                          autovalidateMode: AutovalidateMode.always,
+                          onDateSelected: (DateTime value) {
+                            controller.dateC.text = value.toString();
+                          },
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20),
+                Row(
+                  children: [
+                    Expanded(
+                        child: Text(
+                      "Nomor Kendaraan",
+                      style: TextStyle(
+                          color: Color.fromRGBO(127, 132, 143, 1),
+                          fontSize: 13),
+                    )),
+                    Expanded(
+                      flex: 3,
+                      child: TxField(
+                        hintText: "ex: KB 1678 QU",
+                        controller: controller.carC,
+                        validator: controller.validator,
+                        textCapitalization: TextCapitalization.characters,
+                      ),
+                    ),
+                    SizedBox(height: 25),
+                  ],
+                ),
+                SizedBox(height: 30),
+                StringButton(
+                    color: Colors.white,
+                    pressed: () {
+                      controller.nameC.text.isEmpty &&
+                              controller.priceC.text.isEmpty &&
+                              controller.dateC.text.isEmpty &&
+                              controller.weightC.text.isEmpty &&
+                              controller.carC.text.isEmpty
+                          ? controller.textEmpty
+                          : controller.updateTxDoc();
+                    },
+                    text: "Save"),
+                SizedBox(height: 50),
+              ],
             ),
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
